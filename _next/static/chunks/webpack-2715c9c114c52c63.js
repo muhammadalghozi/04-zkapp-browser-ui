30b892fa514e095e84a01f88a3e959bd6c5c3795
muhammadalghozi <muhammadalghozi@yahoo.com>
2022-12-05 01:08:30 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-2715c9c114c52c63.js
+++ _next/static/chunks/webpack-2715c9c114c52c63.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "4a38d5319ef6362a" + ".css";
+/******/ 			return "static/css/" + "281fbd0c02c4ebb0" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
